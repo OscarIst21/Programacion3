dcbc7f4486c593263c6580328a721d98cb8f627b
Oscar Ist <145413423+OscarIst21@users.noreply.github.com>
2024-02-20 17:20:10 -0700

    unidad 2: ejercicio 14
    Se creo el  menú de inicio de sesión

--- Actividad1/src/Ventana.java
+++ Actividad1/src/Ventana.java
@@ -2,16 +2,18 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class Ventana extends JFrame{
 	//Define los valores base de mi ventana.
 	public Ventana() {
 		this.setTitle("no");
-		this.setVisible(true);
 		this.setSize(920, 560);
 		this.setLocation(120, 120);
 		
@@ -22,6 +24,7 @@ public class Ventana extends JFrame{
 		this.setMaximumSize(new Dimension(600,600));
 		this.setLayout(null);
 		this.iniciarComponentes();
+		this.setVisible(true);
 	}
 	
 	public void iniciarComponentes() {
@@ -31,9 +34,9 @@ public class Ventana extends JFrame{
 		login.setLayout(null);
 		
 		JLabel loginTitulo=new JLabel("Acceder", 0);
-		loginTitulo.setFont(new Font("Arial",Font.BOLD,20));
-		loginTitulo.setSize(300, 80);
-		loginTitulo.setLocation(100, 20);
+		loginTitulo.setFont(new Font("Arial",Font.BOLD,26));
+		loginTitulo.setSize(460, 40);
+		loginTitulo.setLocation(0, 20);
 		loginTitulo.setOpaque(true);
 		loginTitulo.setBackground(Color.cyan);
 		login.add(loginTitulo);
@@ -42,12 +45,40 @@ public class Ventana extends JFrame{
 		JLabel usuario=new JLabel("Nombre de Usuario",0);
 		usuario.setFont(new Font("Arial",Font.BOLD,16));
 		usuario.setSize(150,120);
-		usuario.setLocation(10,120);
+		usuario.setLocation(130,100);
+		usuario.setOpaque(false);
 		login.add(usuario);
 		
-		//JTextField nombreUsuario=new JTextField();
-		//nombreUsuario.setBounds(10, 150, 200, 40);
-		//login.add(nombreUsuario);
+		JTextField nombreUsuario=new JTextField();
+		nombreUsuario.setBounds(130, 180, 200, 30);
+		login.add(nombreUsuario);
+		
+		JLabel contraseña=new JLabel("Contraseña",0);
+		contraseña.setFont(new Font("Arial",Font.BOLD,16));
+		contraseña.setSize(120,120);
+		contraseña.setLocation(115,200);
+		contraseña.setOpaque(false);
+		login.add(contraseña);
+		
+		JPasswordField contraUsuario=new JPasswordField();
+		contraUsuario.setBounds(130, 280, 200, 30);
+		login.add(contraUsuario);
+		
+		JCheckBox recordarme=new JCheckBox("Recordarme");
+		recordarme.setFont(new Font("Arial",Font.BOLD,10));
+		recordarme.setBounds(126, 300, 100, 50);
+		recordarme.setOpaque(false);
+		login.add(recordarme);
+		
+		JLabel olvidoContraseña=new JLabel("¿Olvido su contraseña?");
+		olvidoContraseña.setBounds(226, 310, 140, 30);
+		olvidoContraseña.setFont(new Font("Arial",Font.BOLD,10));
+		olvidoContraseña.setOpaque(false);
+		login.add(olvidoContraseña);
+		
+		JButton iniciar=new JButton("Iniciar Sesion");
+		iniciar.setBounds(160, 410, 140, 30);
+		login.add(iniciar);
 		
 		this.add(login);
 		JPanel registro= new JPanel();

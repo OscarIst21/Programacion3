4d268ae5c3a01643d014a76b2192687580f50e49
Oscar Ist <145413423+OscarIst21@users.noreply.github.com>
2024-02-28 07:30:34 -0800

    unidad 2: ejercicio 24
    Calculadora con layout

---
+++ Actividad1/src/src/Main.java
@@ -0,0 +1,14 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+public class Main extends JFrame{
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 Ventana ventana=new Ventana();
+	}
+	
+
+}

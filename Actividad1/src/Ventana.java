b60c94ba626fb05eeca91ff6cd84c13b1f992624
Oscar Ist <145413423+OscarIst21@users.noreply.github.com>
2024-02-29 13:17:19 -0800

    unidad 2: ejercicio 27
    Calculadora de interes

--- Actividad1/src/Ventana.java
+++ Actividad1/src/Ventana.java
@@ -1,6 +1,9 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridLayout;
 
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
@@ -16,6 +19,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -32,170 +36,308 @@ public class Ventana extends JFrame{
 		
 		this.setMinimumSize(new Dimension(250,250));
 		this.setMaximumSize(new Dimension(600,600));
-		this.setLayout(null);
+		//this.setLayout(null);
 		this.iniciarComponentes();
 		this.setVisible(true);
 	}
 	
 	
 	public void iniciarComponentes() {
-		this.login();
+		//this.login();
 		//this.registro();
 		//this.admin();
 		//this.calculadora();
+		this.calculadoraInteres();
 	}
+	public void calculadoraInteres() {
+	    this.setTitle("Calculadora de Interes");
+	    this.setSize(470, 420);
+	    JPanel panel = new JPanel();
+	    panel.setSize(460, 670);
+	    panel.setLocation(0, 0);
+	    panel.setBackground(new Color(192, 255, 248));
+	    panel.setLayout(new BorderLayout());
+	    
+	    JLabel text = new JLabel("      Interes", 2);
+	    text.setBackground(Color.white);
+	    text.setOpaque(true);
+	    text.setFont(new Font("Arial", Font.BOLD, 26));
+	    text.setForeground(new Color(254, 123, 0));
+	    panel.add(text, BorderLayout.NORTH);
+
+	    JLabel west = new JLabel("             ");
+	    west.setBackground(Color.white);
+	    west.setVisible(true);
+	    west.setOpaque(true);
+	    west.setLayout(new BorderLayout());
+	    panel.add(west, BorderLayout.WEST);
+	    
+	    JLabel east = new JLabel("             ");
+	    east.setBackground(Color.white);
+	    east.setVisible(true);
+	    east.setOpaque(true);
+	    east.setLayout(new BorderLayout());
+	    panel.add(east, BorderLayout.EAST);
+	    
+	    
+	    JLabel sur = new JLabel("             ");
+	    sur.setBackground(Color.white);
+	    sur.setVisible(true);
+	    sur.setOpaque(true);
+	    sur.setLayout(new BorderLayout());
+	    panel.add(sur, BorderLayout.SOUTH);
+	    
+	    JPanel centroP=new JPanel();
+	    centroP.setLayout(new GridLayout(2,1));
+	    panel.add(centroP,BorderLayout.CENTER);
+	    
+	    JPanel centro=new JPanel();
+	    centro.setBackground(new Color(157, 220, 149));
+	    centro.setOpaque(true);
+	    centro.setLayout(new BorderLayout());
+	    centroP.add(centro);
+	    
+	    JLabel text2 = new JLabel(" Calcular interes", 2);
+	    text2.setBackground(new Color(157, 220, 149));
+	    text2.setOpaque(true);
+	    text2.setFont(new Font("Arial", Font.BOLD, 16));
+	    centro.add(text2,BorderLayout.NORTH);
+	    
+	    JPanel contenido6=new JPanel();
+	    contenido6.setLayout(new GridLayout(7,2));
+	    centro.add(contenido6);
+	    
+	    JLabel v=new JLabel("   ");
+	    v.setBackground(new Color(157, 220, 149));
+	    v.setOpaque(true);
+	    contenido6.add(v);
+	    JLabel vv=new JLabel("   ");
+	    vv.setBackground(new Color(157, 220, 149));
+	    vv.setOpaque(true);
+	    contenido6.add(vv);
+	    
+		JLabel textC = new JLabel("    Capaciadad", 2);
+	    textC.setBackground(new Color(157, 220, 149));
+	    textC.setOpaque(true);
+	    textC.setFont(new Font("Arial", Font.BOLD, 12));
+	    contenido6.add(textC);
+
+	    JTextField capacidad=new JTextField();
+	    capacidad.setBounds(130, 180, 200, 30);
+	    capacidad.setHorizontalAlignment(0);
+	    contenido6.add(capacidad);
+	    
+	    JLabel v1=new JLabel("   ");
+	    v1.setBackground(new Color(157, 220, 149));
+	    v1.setOpaque(true);
+	    contenido6.add(v1);
+	    JLabel vv1=new JLabel("   ");
+	    vv1.setBackground(new Color(157, 220, 149));
+	    vv1.setOpaque(true);
+	    contenido6.add(vv1);
+		
+	    JLabel textT = new JLabel("    Tiempo", 2);
+	    textT.setBackground(new Color(157, 220, 149));
+	    textT.setOpaque(true);
+	    textT.setFont(new Font("Arial", Font.BOLD, 12));
+	    contenido6.add(textT);
+
+	    JTextField tiempo=new JTextField();
+	    tiempo.setHorizontalAlignment(0);
+	    contenido6.add(tiempo);
+	    
+	    JLabel v2=new JLabel("   ");
+	    v2.setBackground(new Color(157, 220, 149));
+	    v2.setOpaque(true);
+	    contenido6.add(v2);
+	    JLabel vv2=new JLabel("   ");
+	    vv2.setBackground(new Color(157, 220, 149));
+	    vv2.setOpaque(true);
+	    contenido6.add(vv2);
+		
+		JLabel textTI = new JLabel("    Tasa de Interes", 2);
+	    textTI.setBackground(new Color(157, 220, 149));
+	    textTI.setOpaque(true);
+	    textTI.setFont(new Font("Arial", Font.BOLD, 12));
+	    contenido6.add(textTI);
+
+	    JTextField tasaInteres=new JTextField();
+	    tasaInteres.setHorizontalAlignment(0);
+	    contenido6.add(tasaInteres);
+	    
+	    
+	    
+	    JButton calcular=new JButton("Calcular");
+	    calcular.setBackground(Color.black);
+	    calcular.setForeground(Color.WHITE);
+	    calcular.setBorderPainted(true); //Agregar borde
+		calcular.setBorder(BorderFactory.createLineBorder(new Color(157, 220, 149), 3));
+	    contenido6.add(calcular);
+	    
+	    JButton cancelar=new JButton("Cancelar");
+	    cancelar.setBackground(Color.black);
+	    cancelar.setForeground(Color.WHITE);
+	    cancelar.setBorderPainted(true); //Agregar borde
+		cancelar.setBorder(BorderFactory.createLineBorder(new Color(157, 220, 149), 3));
+	    contenido6.add(cancelar);
+	    
+	    JLabel west2 = new JLabel("                       ");
+	    west2.setBackground(new Color(157, 220, 149));
+	    west2.setVisible(true);
+	    west2.setOpaque(true);
+	    west2.setLayout(new BorderLayout());
+	    centro.add(west2, BorderLayout.WEST);
+	    
+	    JLabel east2 = new JLabel("                       ");
+	    east2.setBackground(new Color(157, 220, 149));
+	    east2.setVisible(true);
+	    east2.setOpaque(true);
+	    east2.setLayout(new BorderLayout());
+	    centro.add(east2, BorderLayout.EAST);
+	    
+	    JPanel panelRosa=new JPanel();
+	    panelRosa.setLayout(new BorderLayout());
+	    centroP.add(panelRosa,BorderLayout.SOUTH);
+	    
+	    JLabel cuadroBlanco=new JLabel(" ");
+	    cuadroBlanco.setBackground(Color.WHITE);
+	    cuadroBlanco.setOpaque(true);
+	    panelRosa.add(cuadroBlanco,BorderLayout.NORTH);
+	    
+	    JLabel cuadroRosa=new JLabel("                       ");
+	    cuadroRosa.setBackground(Color.PINK);
+	    cuadroRosa.setOpaque(true);
+	    panelRosa.add(cuadroRosa,BorderLayout.WEST);
+	    
+	    JLabel cuadroRosa2=new JLabel("                       ");
+	    cuadroRosa2.setBackground(Color.PINK);
+	    cuadroRosa2.setOpaque(true);
+	    panelRosa.add(cuadroRosa2,BorderLayout.EAST);
+	    
+	    
+	    JLabel cuadroRosa4=new JLabel("                       ");
+	    cuadroRosa4.setBackground(Color.WHITE);
+	    cuadroRosa4.setOpaque(true);
+	    panelRosa.add(cuadroRosa4,BorderLayout.SOUTH);
+	    
+	    JPanel cuatroComponenentes=new JPanel();
+	    cuatroComponenentes.setLayout(new GridLayout(5,2));
+	    panelRosa.add(cuatroComponenentes,BorderLayout.CENTER);
+	    
+	    JLabel rosa=new JLabel("   ");
+	    rosa.setBackground(Color.PINK);
+	    rosa.setOpaque(true);
+	    cuatroComponenentes.add(rosa);
+	    JLabel rosa1=new JLabel("   ");
+	    rosa1.setBackground(Color.PINK);
+	    rosa1.setOpaque(true);
+	    cuatroComponenentes.add(rosa1);
+	    
+	    JLabel textMonto = new JLabel("     Interes", 2);
+	    textMonto.setBackground(Color.PINK);
+	    textMonto.setOpaque(true);
+	    textMonto.setFont(new Font("Arial", Font.BOLD, 12));
+	    cuatroComponenentes.add(textMonto);
+
+	    JTextField montoI=new JTextField();
+	    montoI.setHorizontalAlignment(0);
+	    cuatroComponenentes.add(montoI);
+	    
+	    JLabel rosa2=new JLabel("   ");
+	    rosa2.setBackground(Color.PINK);
+	    rosa2.setOpaque(true);
+	    cuatroComponenentes.add(rosa2);
+	    JLabel rosa3=new JLabel("   ");
+	    rosa3.setBackground(Color.PINK);
+	    rosa3.setOpaque(true);
+	    cuatroComponenentes.add(rosa3);
+	    
+	    JLabel textMonto2 = new JLabel("     Monto ", 2);
+	    textMonto2.setBackground(Color.PINK);
+	    textMonto2.setOpaque(true);
+	    textMonto2.setFont(new Font("Arial", Font.BOLD, 12));
+	    cuatroComponenentes.add(textMonto2);
+
+	    JTextField monto=new JTextField();
+	    monto.setHorizontalAlignment(0);
+	    cuatroComponenentes.add(monto);
+
+	    JLabel rosa4=new JLabel("   ");
+	    rosa4.setBackground(Color.PINK);
+	    rosa4.setOpaque(true);
+	    cuatroComponenentes.add(rosa4);
+	    JLabel rosa5=new JLabel("   ");
+	    rosa5.setBackground(Color.PINK);
+	    rosa5.setOpaque(true);
+	    cuatroComponenentes.add(rosa5);
+	    
+	    this.add(panel);
+	}
+
+
+
 	public void calculadora()
 	{
 		this.setTitle("Calculadora");
-		this.setSize(455,670);
-		JPanel calculadoraF= new JPanel();
-		calculadoraF.setSize(460,670);
-		calculadoraF.setLocation(0, 0);
-		calculadoraF.setBackground(Color.BLACK);
-		calculadoraF.setLayout(null);
-		this.add(calculadoraF);
+		this.setSize(465,670);
+		JPanel panel= new JPanel();
+		panel.setSize(460,670);
+		panel.setLocation(0, 0);
+		panel.setBackground(new Color(192, 255, 248));
+		panel.setLayout(new BorderLayout());
 		
-		JLabel panelOP=new JLabel("0", 0);
-		panelOP.setFont(new Font("Impact",Font.BOLD,28));
-		panelOP.setSize(427, 120);
-		panelOP.setForeground(Color.white);
-		panelOP.setLocation(5, 5);
-		panelOP.setOpaque(true);
-		panelOP.setBackground(new Color(49, 49, 49));
-		calculadoraF.add(panelOP);
+		JLabel text=new JLabel("999.00   ",4);
+		text.setBackground(new Color(255,255,255));
+		text.setOpaque(true);
+		text.setFont(new Font("Impact",Font.BOLD,26));
+		panel.add(text,BorderLayout.NORTH);
 		
-		JButton c=new JButton("AC");
-		c.setBounds(5, 130, 103, 95);
-		c.setFont(new Font("Impact",Font.BOLD,22));
-		c.setBackground(new Color(136, 136, 136));
-		c.setForeground(Color.WHITE);
-		calculadoraF.add(c);
 		
-		JButton signo=new JButton("+/-");
-		signo.setBounds(113, 130, 103, 95);
-		signo.setFont(new Font("Impact",Font.BOLD,18));
-		signo.setBackground(new Color(136, 136, 136));
-		signo.setForeground(Color.WHITE);
-		calculadoraF.add(signo);
+		JLabel centro=new JLabel("");
+		centro.setBackground(Color.darkGray);
+		centro.setLayout(new GridLayout(4,3));
+		panel.add(centro,BorderLayout.CENTER);
 		
-		JButton porcentaje=new JButton("%");
-		porcentaje.setBounds(221, 130, 103, 95);
-		porcentaje.setFont(new Font("Impact",Font.BOLD,24));
-		porcentaje.setBackground(new Color(136, 136, 136));
-		porcentaje.setForeground(Color.WHITE);
-		calculadoraF.add(porcentaje);
+		String botones[]= {"7","8","9","4","5","6","1","2","3","0","."};
+		for(int i=0;i<=10;i++)
+		{
+			JButton boton=new JButton(botones[i]);
+			boton.setFont(new Font("Impact",Font.BOLD,26));
+			boton.setFocusable(false);
+			boton.setBackground(new Color(182, 182, 182));
+			centro.add(boton);
+		}
+		JPanel east=new JPanel();
+		east.setBackground(Color.darkGray);
+		east.setLayout(new GridLayout(3,1));
+		panel.add(east,BorderLayout.EAST);
+		this.add(panel);
+		String operadores[]= {"  +  ","  -  ","  =  "};
 		
-		JButton division=new JButton("/");
-		division.setBounds(329, 130, 103, 95);
-		division.setFont(new Font("Impact",Font.BOLD,28));
-		division.setBackground(new Color(207, 122, 0));
-		division.setForeground(Color.WHITE);
-		calculadoraF.add(division);
+		for(int i=0;i<3;i++)
+		{
+			JButton boton=new JButton(operadores[i]);
+			boton.setFont(new Font("Impact",Font.BOLD,26));
+			boton.setFocusable(false);
+			boton.setBackground(new Color(92, 92, 92));
+			east.add(boton);
+		}
+		JPanel west=new JPanel();
+		west.setBackground(Color.darkGray);
+		west.setLayout(new GridLayout(4,1));
+		panel.add(west,BorderLayout.WEST);
+		this.add(panel);
+		String op[]= {"AC","DEL","MC","M+"};
 		
-		JButton siete=new JButton("7");
-		siete.setBounds(5, 230, 103, 95);
-		siete.setFont(new Font("Impact",Font.BOLD,28));
-		siete.setBackground(new Color(41, 41, 41));
-		siete.setForeground(Color.WHITE);
-		calculadoraF.add(siete);
+		for(int i=0;i<4;i++)
+		{
+			JButton boton=new JButton(op[i]);
+			boton.setFont(new Font("Impact",Font.BOLD,26));
+			boton.setFocusable(false);
+			boton.setBackground(new Color(231, 124, 31));
+			west.add(boton);
+		}
 		
-		JButton ocho=new JButton("8");
-		ocho.setBounds(113, 230, 103, 95);
-		ocho.setFont(new Font("Impact",Font.BOLD,28));
-		ocho.setBackground(new Color(41, 41, 41));
-		ocho.setForeground(Color.WHITE);
-		calculadoraF.add(ocho);
-		
-		JButton nueve=new JButton("9");
-		nueve.setBounds(221, 230, 103, 95);
-		nueve.setFont(new Font("Impact",Font.BOLD,28));
-		nueve.setBackground(new Color(41, 41, 41));
-		nueve.setForeground(Color.WHITE);
-		calculadoraF.add(nueve);
-		
-		JButton multiplicacion=new JButton("X");
-		multiplicacion.setBounds(329, 230, 103, 95);
-		multiplicacion.setFont(new Font("Impact",Font.BOLD,28));
-		multiplicacion.setBackground(new Color(207, 122, 0));
-		multiplicacion.setForeground(Color.WHITE);
-		calculadoraF.add(multiplicacion);
-		
-		JButton cuatro=new JButton("4");
-		cuatro.setBounds(5, 330, 103, 95);
-		cuatro.setFont(new Font("Impact",Font.BOLD,28));
-		cuatro.setBackground(new Color(41, 41, 41));
-		cuatro.setForeground(Color.WHITE);
-		calculadoraF.add(cuatro);
-		
-		JButton cinco=new JButton("5");
-		cinco.setBounds(113, 330, 103, 95);
-		cinco.setFont(new Font("Impact",Font.BOLD,28));
-		cinco.setBackground(new Color(41, 41, 41));
-		cinco.setForeground(Color.WHITE);
-		calculadoraF.add(cinco);
-		
-		JButton seis=new JButton("6");
-		seis.setBounds(221, 330, 103, 95);
-		seis.setFont(new Font("Impact",Font.BOLD,28));
-		seis.setBackground(new Color(41, 41, 41));
-		seis.setForeground(Color.WHITE);
-		calculadoraF.add(seis);
-		
-		JButton resta=new JButton("-");
-		resta.setBounds(329, 330, 103, 95);
-		resta.setFont(new Font("Impact",Font.BOLD,28));
-		resta.setBackground(new Color(207, 122, 0));
-		resta.setForeground(Color.WHITE);
-		calculadoraF.add(resta);
-		
-		JButton uno=new JButton("1");
-		uno.setBounds(5, 430, 103, 95);
-		uno.setFont(new Font("Impact",Font.BOLD,28));
-		uno.setBackground(new Color(41, 41, 41));
-		uno.setForeground(Color.WHITE);
-		calculadoraF.add(uno);
-		
-		JButton dos=new JButton("2");
-		dos.setBounds(113, 430, 103, 95);
-		dos.setFont(new Font("Impact",Font.BOLD,28));
-		dos.setBackground(new Color(41, 41, 41));
-		dos.setForeground(Color.WHITE);
-		calculadoraF.add(dos);
-		
-		JButton tres=new JButton("3");
-		tres.setBounds(221, 430, 103, 95);
-		tres.setFont(new Font("Impact",Font.BOLD,28));
-		tres.setBackground(new Color(41, 41, 41));
-		tres.setForeground(Color.WHITE);
-		calculadoraF.add(tres);
-		
-		JButton suma=new JButton("+");
-		suma.setBounds(329, 430, 103, 95);
-		suma.setFont(new Font("Impact",Font.BOLD,28));
-		suma.setBackground(new Color(207, 122, 0));
-		suma.setForeground(Color.WHITE);
-		calculadoraF.add(suma);
-		
-		JButton cero=new JButton("0");
-		cero.setBounds(5, 530, 210, 95);
-		cero.setFont(new Font("Impact",Font.BOLD,28));
-		cero.setBackground(new Color(41, 41, 41));
-		cero.setForeground(Color.WHITE);
-		calculadoraF.add(cero);
-		
-		JButton punto=new JButton(".");
-		punto.setBounds(221, 530, 103, 95);
-		punto.setFont(new Font("Impact",Font.BOLD,28));
-		punto.setBackground(new Color(41, 41, 41));
-		punto.setForeground(Color.WHITE);
-		calculadoraF.add(punto);
-		
-		JButton igual=new JButton("=");
-		igual.setBounds(329, 530, 103, 95);
-		igual.setFont(new Font("Impact",Font.BOLD,28));
-		igual.setBackground(new Color(207, 122, 0));
-		igual.setForeground(Color.WHITE);
-		calculadoraF.add(igual);
 	}
 	public void admin() {
 		//añadir panel

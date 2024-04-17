a7a4af27a13abedd3cd8cc8fd26219964986332a
Oscar Ist <145413423+OscarIst21@users.noreply.github.com>
2024-04-17 07:51:23 -0700

    unidad 3 ejercicio 25

--- Actividad1/src/TableroDibujo.java
+++ Actividad1/src/TableroDibujo.java
@@ -2,20 +2,23 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
 
 public class TableroDibujo extends JFrame implements KeyListener {
 
     private JPanel panel;
-
+    int x=260;
+    int y=310;
     public TableroDibujo() {
         setTitle("Tablero de Dibujo");
         setSize(560, 720);
-        setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setMinimumSize(new Dimension(250, 250));
         setMaximumSize(new Dimension(600, 600));
@@ -33,16 +36,31 @@ public class TableroDibujo extends JFrame implements KeyListener {
             @Override
             public void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                g.setColor(Color.RED);
-                g.fillRect(0, 0, 560, 660);
+                g.setColor(new Color(28, 184, 28));
+                g.fillRect(x, y, 40, 40);
             }
         };
+        tablero.setBorder(new LineBorder(new Color(23, 36, 108),10));
+        tablero.setBackground(new Color(0, 0, 0));
         panel.add(tablero, BorderLayout.CENTER);
 
         JButton reiniciar = new JButton("Reiniciar");
         reiniciar.setFocusable(false);
+        reiniciar.addActionListener(new ActionListener()
+		{
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				 x=260;
+			     y=310;
+			}
+        	
+		});
         JPanel botonesPanel = new JPanel();
+        botonesPanel.setBackground(new Color(23, 36, 108));
         botonesPanel.add(reiniciar);
+        
         panel.add(botonesPanel, BorderLayout.SOUTH);
 
         this.add(panel);
@@ -55,6 +73,21 @@ public class TableroDibujo extends JFrame implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         System.out.println("Tecla Presionada: " + e.getKeyCode());
+        switch(e.getKeyCode()) {
+        case 37:
+        	x-=10;
+        	break;
+        case 38:
+        	y-=10;
+            break;
+        case 39:
+        	x+=10;
+            break;
+        case 40:
+        	y+=10;
+            break;
+        }
+        this.repaint();
     }
 
     @Override

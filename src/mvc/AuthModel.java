53f7b4fcf238ae6ce16c83b1f37c9b3362a96283
OscarIst21 <oscaruriel_22@alu.uabcs.mx>
2024-05-03 15:09:36 -0700

    unidad 4 ejercicio 9

--- src/mvc/AuthModel.java
+++ src/mvc/AuthModel.java
@@ -1,14 +1,38 @@
 package mvc;
 
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 public class AuthModel {
 	public AuthModel() {
 		
 	}
 	public boolean login(String username,String password) {
-		if(username.equals("admin") && password.equals("123"))
-		{
-			return true;
-		}
-		return false;
+			
+	        try {
+	            FileReader reader = new FileReader("src/usuario.json");
+	            JSONObject jsonObject = new JSONObject(new JSONTokener(reader));
+	            String user = jsonObject.getString("usuario");
+	            String contrasena = jsonObject.getString("contrasena");
+	            reader.close();
+	            if (username.equals(user) && password.equals(contrasena)) {
+	                //JOptionPane.showMessageDialog(null, "Bienvenido");
+	                return true;
+	            } else {
+	            	//JOptionPane.showMessageDialog(null, "Usuario no encontrado");
+	                return false;
+	            }
+	        } catch (IOException ex) {
+	            ex.printStackTrace();
+	        } catch (JSONException ex) {
+	            ex.printStackTrace();
+	        }
+	        return false;
 	}
 }
